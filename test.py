9c809ab365bd0ecc1408f86127c95c06b6e591e2
Gavin Grochowski <grochowskigavin@gmail.com>
2020-09-07 18:05:51 -0700

    Made Workable Schedule_study_reminders function for Scheduler Class

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
 import unittest
-from things_trials import *
+from things_classes import ThingsMaker, ThingsStudyScheduler
 
 
 class TestThingsMaker(unittest.TestCase):
     def setUp(self):
-        self.things_maker_object = ThingsMaker()
+        self.things_maker = ThingsMaker()
 
     def test_things_maker_traits(self):
-        self.assertEqual(str, type(self.things_maker_object.addlink))
+        self.assertEqual(str, type(self.things_maker.addlink))
+
+
+class TestStudyScheduler(unittest.TestCase):
+    def setUp(self):
+        title = "This is a test to do"
+        project = "Testing Project"
+        link = "https:\\fakelinkstring"
+        self.study_scheduler = ThingsStudyScheduler(title, project, link)
+
+    def test_things_study_scheduler_init(self):
+        self.assertNotEqual(len(self.study_scheduler.addlink), 0)
+        self.assertEqual(str, type(self.study_scheduler.title))
+        self.assertEqual(str, type(self.study_scheduler.project))
+        self.assertIn("Do Quick", self.study_scheduler.tags)
+        self.assertEqual(len(self.study_scheduler.spaced_repetition_gaps), 16)
+        self.assertTrue(self.study_scheduler.gap_title_dict["1 day"])
 
 
 if __name__ == "__main__":
---
+++ things_classes.py
@@ -0,0 +1,99 @@
+import webbrowser
+
+
+class ThingsMaker:
+    """
+    A class to centralize the automation of things tasks.
+    Currently only used with the ThingsStudyModule() subclass.
+
+    Left an open framework for future automations
+    """
+
+    addlink = "things:///add?title="
+
+
+class ThingsStudyScheduler(ThingsMaker):
+    """
+    A class to setup spaced repition reminders in Things for studying.
+
+
+    Parameters
+    **********
+    title = The title of the lecture you want to review
+    project = The things Project that the reminders will populate in.
+    link = An optional link to your page of notes in Notion or Bear.
+
+
+    Methods
+    *******
+    make_spaced_repition_reminders()
+        =   Uses a spaced repetition series of dates and things 3's natural
+            language parsing, and generates a series of 16 reminder todo's
+            into the future with increasingly larger gaps between them.
+
+    """
+
+    spaced_repetition_gaps = [
+        "1 day",
+        "1 days",
+        "2 days",
+        "3 days",
+        "5 days",
+        "7 days",
+        "10 days",
+        "2 weeks",
+        "3 weeks",
+        "5 weeks",
+        "2 months",
+        "3 months",
+        "5 months",
+        "7 months",
+        "10 months",
+        "1 year",
+    ]
+
+    def __init__(self, title, project, link=None):
+        self.title = title
+        self.project = project
+        if link:
+            self.link = link
+        else:
+            self.link = ""
+        self.tags = "%F0%9F%A4%A9 Do Quick %F0%9F%9A%80"
+
+        # Making the gap-title Dictionary for the reminders function.
+        # Placing this here rather than in the next function so I can unit test.
+        day1 = "Review: Make {} into Notion Categories - {} later.".format(
+            self.title, self.spaced_repetition_gaps[0]
+        )
+        day2 = "Review: Make {} into Notion Quiz Questions - {} later.".format(
+            self.title, self.spaced_repetition_gaps[1]
+        )
+        title_list = [day1, day2]
+        for gap in self.spaced_repetition_gaps[2:]:
+            title_list.append("Review: {} - {} later".format(self.title, gap))
+        self.gap_title_dict = {
+            gap: title for gap, title in zip(self.spaced_repetition_gaps, title_list)
+        }
+
+    def schedule_study_reminders(self):
+        """
+        Generates a series of ToDo's in Things 3 with the given title,
+        project, and description (link) at the Heading equal to the title, all
+        spaced out according to spaced repition.
+        """
+        for gap, title in self.gap_title_dict.items():
+            webbrowser.open(
+                self.addlink
+                + title.replace(" ", "%20").replace(":", "%3A")
+                + "&notes="  # &notes=
+                + self.link.replace(":", "%3A")
+                + "&when="
+                + gap.replace(" ", "%20").replace(":", "%3A")
+                + "&tags="
+                + self.tags.replace(" ", "%20")
+                + "&list="
+                + self.project.replace(" ", "%20").replace(":", "%3A")
+                + "&heading="
+                + self.title.replace(" ", "%20").replace(":", "%3A")
+            )
