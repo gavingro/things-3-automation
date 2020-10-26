ce4947e34cf0af29e3d6cd14fedcaf6de9f5914e
Gavin Grochowski <grochowskigavin@gmail.com>
2020-10-26 10:14:23 -0700

    use case

--- quick_generator.py
+++ quick_generator.py
@@ -5,11 +5,11 @@
 
 from things_classes import ThingsStudyScheduler
 
-lecture_notes_title = """L13 - Intro to Classes and Objects (S13 to 140)"""
-class_title_project = (
-    """Review: CPSC 1113 - Principles of Program Structure and Design"""
+lecture_notes_title = (
+    """L13 - Discrete Random Variables and Linear Combinations (3.4)"""
 )
-notion_url = """https://www.notion.so/L13-Intro-to-Classes-and-Objects-S13-to-140-73573deb6ee04801a031c7f34dd4c42c"""
+class_title_project = """Review: STAT 101 - Introduction to Statistics"""
+notion_url = """https://www.notion.so/L13-Discrete-Random-Variables-and-Linear-Combinations-3-4-b2cd25095f4c40f6be2225401b86eacd"""
 notion_url = notion_url.replace("https:", "notion:")
 
 study_test = ThingsStudyScheduler(lecture_notes_title, class_title_project, notion_url)
