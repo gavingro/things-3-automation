a9b9e947f7e3ed34edda6fc8c1c7bcf3c33caf72
Gavin Grochowski <grochowskigavin@gmail.com>
2021-01-11 20:59:47 -0800

    Update use-case to main.py

--- main.py
+++ main.py
@@ -4,17 +4,22 @@
 
 # Then, when you're ready, run the script.
 
-from things_classes import ThingsStudyScheduler
+from things_study_scheduler import ThingsStudyScheduler
 
-lecture_notes_title = """L1 - Working with Vectors"""
-class_title_project = """Review: MATH 152 - Linear Algebra"""
-notion_url = (
+# Input the name of the lecture notes here:
+reminder_title = """TESTINGTHINGSTODO"""
+
+# Input the name of the class or project area here:
+project_area = """Review: MATH 152 - Linear Algebra"""
+
+# Input the description or link you want to attach on the to-do here:
+desc = (
     """https://www.notion.so/L1-Working-with-Vectors-8a06aa5ef5074ea7ab4bd3992ca7dfff"""
 )
-notion_url = notion_url.replace("https:", "notion:")
+if "www.notion.so" in desc:
+    desc = desc.replace("https:", "notion:")
 
-study_test = ThingsStudyScheduler(lecture_notes_title, class_title_project, notion_url)
-
+review_scheduler = ThingsStudyScheduler(reminder_title, project_area, desc)
 
 if __name__ == "__main__":
-    study_test.schedule_study_reminders()
+    review_scheduler.schedule_study_reminders()
