f38effc95435d687f0703e726ce9d251450e2940
Gavin Grochowski <grochowskigavin@gmail.com>
2021-01-11 20:53:31 -0800

    Separating Class Files, Modularising Methods, and Updating Unit Tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
-from things_classes import ThingsMaker, ThingsStudyScheduler
+from things_maker import ThingsMaker
+from things_study_scheduler import ThingsStudyScheduler
 
 
 class TestThingsMaker(unittest.TestCase):
@@ -7,23 +8,36 @@ class TestThingsMaker(unittest.TestCase):
         self.things_maker = ThingsMaker()
 
     def test_things_maker_traits(self):
-        self.assertEqual(str, type(self.things_maker.addlink))
+        self.assertEqual(str, type(self.things_maker.things_url))
 
 
 class TestStudyScheduler(unittest.TestCase):
     def setUp(self):
         title = "This is a test to do"
         project = "Testing Project"
-        link = "https:\\fakelinkstring"
-        self.study_scheduler = ThingsStudyScheduler(title, project, link)
+        desc = "https:\\fakelinkstring for studying"
+        self.study_scheduler = ThingsStudyScheduler(title, project, desc)
 
     def test_things_study_scheduler_init(self):
-        self.assertNotEqual(len(self.study_scheduler.addlink), 0)
+        self.assertNotEqual(len(self.study_scheduler.things_url), 0)
         self.assertEqual(str, type(self.study_scheduler.title))
         self.assertEqual(str, type(self.study_scheduler.project))
         self.assertIn("Do Quick", self.study_scheduler.tags)
         self.assertEqual(len(self.study_scheduler.spaced_repetition_gaps), 16)
         self.assertTrue(self.study_scheduler.gap_title_dict["1 day"])
+
+    def test_things_study_scheduler_clean_attributes(self):
+        self.study_scheduler.clean_attributes()
+        self.assertNotIn(" ", self.study_scheduler.title)
+        self.assertNotIn(":", self.study_scheduler.desc)
+        self.assertNotIn(" ", self.study_scheduler.project)
+
+    def test_things_generate_link(self):
+        self.study_scheduler.clean_attributes()
+        url = self.study_scheduler.get_generator_link()
+        self.assertTrue(url)
+        self.assertNotIn(" ", url)
+        self.assertIn("things:///add?", url)
 
 
 if __name__ == "__main__":
--- things_study_scheduler.py
+++ things_study_scheduler.py
@@ -1,15 +1,5 @@
 import webbrowser
-
-
-class ThingsMaker:
-    """
-    A class to centralize the automation of things tasks.
-    Currently only used with the ThingsStudyModule() subclass.
-
-    Left an open framework for future automations
-    """
-
-    addlink = "things:///add?title="
+from things_maker import ThingsMaker
 
 
 class ThingsStudyScheduler(ThingsMaker):
@@ -19,14 +9,14 @@ class ThingsStudyScheduler(ThingsMaker):
 
     Parameters
     **********
-    title = The title of the lecture you want to review
+    title = The title of the thing that you want to review.
     project = The things Project that the reminders will populate in.
-    link = An optional link to your page of notes in Notion or Bear.
+    desc = An optional desc to your page of notes in Notion or Bear.
 
 
     Methods
     *******
-    make_spaced_repition_reminders()
+    schedule_study_reminders()
         =   Uses a spaced repetition series of dates and things 3's natural
             language parsing, and generates a series of 16 reminder todo's
             into the future with increasingly larger gaps between them.
@@ -52,13 +42,13 @@ class ThingsStudyScheduler(ThingsMaker):
         "1 year",
     ]
 
-    def __init__(self, title, project, link=None):
+    def __init__(self, title, project, desc=None):
         self.title = title
         self.project = project
-        if link:
-            self.link = link
+        if desc:
+            self.desc = desc
         else:
-            self.link = ""
+            self.desc = ""
         self.tags = "%F0%9F%A4%A9 Do Quick %F0%9F%9A%80"
 
         # Making the gap-title Dictionary for the reminders function.
@@ -76,24 +66,50 @@ class ThingsStudyScheduler(ThingsMaker):
             gap: title for gap, title in zip(self.spaced_repetition_gaps, title_list)
         }
 
+    def clean_attributes(self):
+        """
+        Replaces characters that could cause errors when opening the things url
+        link in all the class attributes needed to create the to-do's.
+        """
+        bad_characters = {
+            " ": "%20",
+            ":": "%3A",
+        }
+        for character, replacement in bad_characters.items():
+            self.title = self.title.replace(character, replacement)
+            self.desc = self.desc.replace(character, replacement)
+            self.tags = self.tags.replace(character, replacement)
+            self.project = self.project.replace(character, replacement)
+            # TODO: How can we replace this with a list? Pass by reference?
+
+    def get_generator_link(self, title="Untitled", gap="in 1 day"):
+        """
+        Uses the cleaned attributes, the things url scheme link, and the
+        passed in to-do title to return a url link that when opened, will
+        generate the necessary todo.
+        """
+        generator_url = (
+            self.things_url
+            + title.replace(" ", "%20").replace(":", "%3A")
+            + "&notes="
+            + self.desc
+            + "&when="
+            + gap.replace(" ", "%20").replace(":", "%3A")
+            + "&tags="
+            + self.tags
+            + "&list="
+            + self.project
+            + "&heading="
+            + self.title
+        )
+        return generator_url
+
     def schedule_study_reminders(self):
         """
         Generates a series of ToDo's in Things 3 with the given title,
-        project, and description (link) at the Heading equal to the title, all
+        project, and description (desc) at the Heading equal to the title, all
         spaced out according to spaced repition.
         """
+        self.clean_attributes()
         for gap, title in self.gap_title_dict.items():
-            webbrowser.open(
-                self.addlink
-                + title.replace(" ", "%20").replace(":", "%3A")
-                + "&notes="
-                + self.link.replace(":", "%3A")
-                + "&when="
-                + gap.replace(" ", "%20").replace(":", "%3A")
-                + "&tags="
-                + self.tags.replace(" ", "%20")
-                + "&list="
-                + self.project.replace(" ", "%20").replace(":", "%3A")
-                + "&heading="
-                + self.title.replace(" ", "%20").replace(":", "%3A")
-            )
+            webbrowser.open(self.get_generator_link(title, gap))
