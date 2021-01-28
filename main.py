b4e45ad93fcd20116949b9fcf68612f2f0c23727
Gavin Grochowski <grochowskigavin@gmail.com>
2021-01-27 19:29:36 -0800

    Public Use Example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Use this program in your text editor to create spaced repetition to-do's.
-# Fill in your Lecture Notes Title (Header), Class (Things Project) Name, and
+# Fill in your reminder title (Header), area (Things Project) Name, and
 #       description to add to each to-do.
 
 # Then, when you're ready, run the script.
@@ -7,15 +7,13 @@
 from things_study_scheduler import ThingsStudyScheduler
 
 # Input the name of the lecture notes here:
-reminder_title = """TESTINGTHINGSTODO"""
+reminder_title = """Topic 2: Electric Boogaloo"""
 
 # Input the name of the class or project area here:
-project_area = """Review: MATH 152 - Linear Algebra"""
+project_area = """BLAT 9000"""
 
 # Input the description or link you want to attach on the to-do here:
-desc = (
-    """https://www.notion.so/L1-Working-with-Vectors-8a06aa5ef5074ea7ab4bd3992ca7dfff"""
-)
+desc = """You love blat class, just remember to check in on topic 2. https://www.notion.so/BLAT9000"""
 if "www.notion.so" in desc:
     desc = desc.replace("https:", "notion:")
 
